56c025da6a35b24a834c2d17257181ef6931b541
SJ <soonjae.jung@chexcar.co.kr>
2025-02-17 09:09:52 +0900

    MOS -> HIVE 변경

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -65,7 +65,7 @@ export class AppController {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `:building_construction: MOS 데일리_${DateTime.now().toFormat('yyyy_MM_dd')}(${weekdayKoreanMapper[DateTime.now().weekday]})`,
+            text: `:building_construction: HIVE 데일리_${DateTime.now().toFormat('yyyy_MM_dd')}(${weekdayKoreanMapper[DateTime.now().weekday]})`,
           },
         },
         {
